fc319bd6858c669e8fa9aa7619c4842a15054eb2
Eugene Lozovoy <uzix.ls@gmail.com>
2022-11-15 20:44:01 +0300

    update cpld firmware - finally working version

--- cpld/rtl/top.v
+++ cpld/rtl/top.v
@@ -3,22 +3,24 @@ module zx_multisound(
     input clk32,
     input clkx,
 
-    input [2:0] cfg,
+    input [4:0] cfg,
 
     input [15:0] a,
     inout [7:0] d,
     input n_rd,
     input n_wr,
     input n_iorq,
+    input n_mreq,
     input n_m1,
     output n_wait,
     output n_iorqge,
 
-    input dos,
-    input iodos,
+    input n_dos,
+    input n_iodos,
 
     output aa0,
     inout [7:0] ad,
+    output n_rstout,
     output n_ard,
     output n_awr,
     output ym_m,
@@ -41,22 +43,44 @@ module zx_multisound(
     input n_gmreq,
     input n_giorq,
     output n_grom,
-    output n_gram,
+    output n_gram1,
+    output n_gram2,
     output [18:15] gma,
 
-    output gdac0,
-    output gdac1,
-    output gdac2,
-    output gdac3
+    output dac0_out,
+    output dac1_out,
+    output dac2_out,
+    output dac3_out
 );
 
-wire n_iorq0 = n_iorq | ~dos | ~iodos;
+assign n_rstout = rst_n;
+
+// n_iorq are useless in zxevo :(
+// so we're detecting n_iorq cycle by n_rd/n_wr signal asserted without n_m1/n_mreq
+reg ioreq;
+always @(negedge clk32) begin
+    // ioreq <= n_iorq == 1'b0 && n_m1 == 1'b1 && n_dos == 1'b1 && n_iodos == 1'b1;
+    ioreq <= n_m1 == 1'b1 && n_mreq == 1'b1 && (n_rd == 1'b0 || n_wr == 1'b0);
+end
+wire ioreq_rd = ioreq && n_rd == 1'b0;
+wire ioreq_wr = ioreq && n_wr == 1'b0;
+
+// n_dos are useless in zxevo :(
+// so we're just lock some ports access when instruction has been fetched from rom
+reg rom_m1_access;
+always @(negedge clk32 or negedge rst_n) begin
+    if (!rst_n)
+        rom_m1_access <= 0;
+    else if (n_m1 == 0)
+        rom_m1_access <= a[15:14] == 2'b00;
+end
 
 
 /* CONFIGURATION */
-wire ym_ena = cfg[0];
+wire ym_ena  = cfg[0];
 wire saa_ena = cfg[1];
-wire gs_ena = cfg[2];
+wire gs_ena  = cfg[2];
+wire sd_ena  = cfg[3];
 
 
 /* TURBO SOUND FM */
@@ -65,8 +89,8 @@ wire port_fffd      = a[15:14] == 2'b11  && a[1:0] == 2'b01 && ym_ena;
 wire port_fffd_full = a[15:13] == 3'b111 && a[1:0] == 2'b01 && ym_ena; // required for compatibility with #dffd port
 reg ym_chip_sel, ym_get_stat;
 wire ym_a0 = (~n_rd & a[14] & ~ym_get_stat) | (~n_wr & ~a[14]);
-assign n_ym1_cs = ~(~ym_chip_sel && (port_bffd || port_fffd) && ~n_iorq0 && n_m1);
-assign n_ym2_cs = ~( ym_chip_sel && (port_bffd || port_fffd) && ~n_iorq0 && n_m1);
+assign n_ym1_cs = ~(~ym_chip_sel && (port_bffd || port_fffd));
+assign n_ym2_cs = ~( ym_chip_sel && (port_bffd || port_fffd));
 
 always @(posedge clk32 or negedge rst_n) begin
     if (!rst_n) begin
@@ -75,7 +99,7 @@ always @(posedge clk32 or negedge rst_n) begin
         fm1_ena <= 0;
         fm2_ena <= 0;
     end
-    else if (port_fffd && ~n_iorq0 && ~n_wr && d[7:3] == 5'b11111) begin
+    else if (port_fffd && ioreq_wr && d[7:4] == 4'b1111) begin
         ym_chip_sel <= ~d[0];
         ym_get_stat <= ~d[1];
         fm1_ena <= d[2]? 1'b0 : 1'bz;
@@ -91,16 +115,17 @@ end
 
 
 /* SAA1099 */
-wire port_ff = a[7:0] == 8'hFF && saa_ena;
-assign n_saa_cs = ~(port_ff && ~n_iorq0 && ~n_wr);
+wire port_ff = a[7:0] == 8'hFF && saa_ena && !rom_m1_access;
+assign n_saa_cs = ~(port_ff && ioreq_wr);
 wire saa_a0 = a[8];
 
+wire port_fffd_saa = a[15:14] == 2'b11 && a[1:0] == 2'b01 && saa_ena;
 reg saa_clk_en;
-always @(posedge clk32) begin
+always @(posedge clk32 or negedge rst_n) begin
     if (!rst_n)
-        saa_clk_en <= 1'b0;
-    else if (~n_saa_cs && ~n_awr)
-        saa_clk_en <= 1'b1;
+        saa_clk_en <= 0;
+    else if (port_fffd_saa && ioreq_wr && d[7:4] == 4'b1111)
+        saa_clk_en <= ~d[3];
 end
 
 reg [1:0] saa_clk_cnt = 0;
@@ -120,7 +145,7 @@ end
 
 /* GENERAL SOUND */
 assign gclk = midi_clk;
-assign n_grst = rst_n;
+assign n_grst = n_rstout;
 
 reg [8:0] g_int_cnt;
 wire g_int_reload = g_int_cnt[8:6] == 4'b101;
@@ -152,108 +177,72 @@ always @(posedge clk32 or negedge rst_n) begin
         gs_regbb <= 0;
     end
     else begin
-        if (port_b3 && ~n_iorq0 && ~n_wr)
+        if (port_b3 && ioreq_wr)
             gs_regb3 <= d;
-        if (port_bb && ~n_iorq0 && ~n_wr)
+        if (port_bb && ioreq_wr)
             gs_regbb <= d;
     end
 end
 
 /* GS INTERNAL REGISTERS */
 reg [7:0] gs_reg00, gs_reg03;
-wire [3:0] gs_page = gs_reg00[3:0];
-reg [5:0] gs_vol0, gs_vol1, gs_vol2, gs_vol3;
+wire [5:0] gs_page = gs_reg00[5:0];
 always @(posedge clk32 or negedge rst_n) begin
     if (!rst_n) begin
         gs_reg00 <= 0;
         gs_reg03 <= 0;
-        gs_vol0 <= 0;
-        gs_vol1 <= 0;
-        gs_vol2 <= 0;
-        gs_vol3 <= 0;
     end
     else if (~n_giorq && ~n_gwr) begin
         if (ga[3:0] == 4'h0) gs_reg00 <= gd;
         if (ga[3:0] == 4'h3) gs_reg03 <= gd;
-        if (ga[3:0] == 4'h6) gs_vol0 <= gd[5:0];
-        if (ga[3:0] == 4'h7) gs_vol1 <= gd[5:0];
-        if (ga[3:0] == 4'h8) gs_vol2 <= gd[5:0];
-        if (ga[3:0] == 4'h9) gs_vol3 <= gd[5:0];
     end
 end
 
-reg [7:0] gs_dac0, gs_dac1, gs_dac2, gs_dac3;
-always @(posedge clk32 or negedge rst_n) begin
-    if (!rst_n) begin
-        gs_dac0 <= 0;
-        gs_dac1 <= 0;
-        gs_dac2 <= 0;
-        gs_dac3 <= 0;
-    end
-    else if (~n_gmreq && ~n_grd && ga[15:13] == 3'b011) begin
-        if (ga[9:8] == 2'b00) gs_dac0 <= gd;
-        if (ga[9:8] == 2'b01) gs_dac1 <= gd;
-        if (ga[9:8] == 2'b10) gs_dac2 <= gd;
-        if (ga[9:8] == 2'b11) gs_dac3 <= gd;
-    end
-end
+/* GS DAC REGISTERS */
+reg gs_vol0_cs; always @(posedge clk32) gs_vol0_cs = ~n_giorq && ga[3:0] == 4'h6;
+reg gs_vol1_cs; always @(posedge clk32) gs_vol1_cs = ~n_giorq && ga[3:0] == 4'h7;
+reg gs_vol2_cs; always @(posedge clk32) gs_vol2_cs = ~n_giorq && ga[3:0] == 4'h8;
+reg gs_vol3_cs; always @(posedge clk32) gs_vol3_cs = ~n_giorq && ga[3:0] == 4'h9;
+reg gs_dac0_cs; always @(posedge clk32) gs_dac0_cs = ~n_gmreq && ga[15:13] == 3'b011 && ga[9:8] == 2'd0;
+reg gs_dac1_cs; always @(posedge clk32) gs_dac1_cs = ~n_gmreq && ga[15:13] == 3'b011 && ga[9:8] == 2'd1;
+reg gs_dac2_cs; always @(posedge clk32) gs_dac2_cs = ~n_gmreq && ga[15:13] == 3'b011 && ga[9:8] == 2'd2;
+reg gs_dac3_cs; always @(posedge clk32) gs_dac3_cs = ~n_gmreq && ga[15:13] == 3'b011 && ga[9:8] == 2'd3;
+wire gs_vol0_wr = gs_vol0_cs && ~n_gwr;
+wire gs_vol1_wr = gs_vol1_cs && ~n_gwr;
+wire gs_vol2_wr = gs_vol2_cs && ~n_gwr;
+wire gs_vol3_wr = gs_vol3_cs && ~n_gwr;
+wire gs_dac0_wr = gs_dac0_cs && ~n_grd;
+wire gs_dac1_wr = gs_dac1_cs && ~n_grd;
+wire gs_dac2_wr = gs_dac2_cs && ~n_grd;
+wire gs_dac3_wr = gs_dac3_cs && ~n_grd;
 
 /* GS STATUS REGISTER */
-reg gs_status0, gs_status7;
-wire [7:0] gs_status = {gs_status7, 6'b111111, gs_status0};
+reg gs_flag_cmd, gs_flag_data;
+wire [7:0] gs_status = {gs_flag_data, 6'b111111, gs_flag_cmd};
 
 always @(posedge clk32) begin
-    if ((~n_giorq && n_gm1 && ga[3:0] == 4'h2) || (~n_iorq0 && ~n_rd && port_b3))
-        gs_status7 <= 1'b0;
-    else if ((~n_giorq && n_gm1 && ga[3:0] == 4'h3) || (~n_iorq0 && ~n_wr && port_b3))
-        gs_status7 <= 1'b1;
+    if ((~n_giorq && n_gm1 && ga[3:0] == 4'h2) || (ioreq_rd && port_b3))
+        gs_flag_data <= 1'b0;
+    else if ((~n_giorq && n_gm1 && ga[3:0] == 4'h3) || (ioreq_wr && port_b3))
+        gs_flag_data <= 1'b1;
     else if (~n_giorq && n_gm1 && ga[3:0] == 4'hA)
-        gs_status7 <= ~gs_reg00[0];
+        gs_flag_data <= ~gs_reg00[0];
 end
 
 always @(posedge clk32) begin
     if (~n_giorq && n_gm1 && ga[3:0] == 4'h5)
-        gs_status0 <= 1'b0;
-    else if (~n_iorq0 && ~n_wr && port_bb)
-        gs_status0 <= 1'b1;
+        gs_flag_cmd <= 1'b0;
+    else if (ioreq_wr && port_bb)
+        gs_flag_cmd <= 1'b1;
     else if (~n_giorq && n_gm1 && ga[3:0] == 4'hB)
-        gs_status0 <= gs_vol0[5];
-end
-
-/* GS DAC */
-reg vol0_en, vol1_en, vol2_en, vol3_en;
-reg [5:0] vol_cnt;
-reg [8:0] dac0_cnt, dac1_cnt, dac2_cnt, dac3_cnt;
-assign gdac0 = dac0_cnt[8];
-assign gdac1 = dac1_cnt[8];
-assign gdac2 = dac2_cnt[8];
-assign gdac3 = dac3_cnt[8];
-always @(posedge clk32 or negedge rst_n) begin
-    if (!rst_n) begin
-        {vol0_en, vol1_en, vol2_en, vol3_en} <= 0;
-        vol_cnt <= 0;
-        dac0_cnt <= 0;
-        dac1_cnt <= 0;
-        dac2_cnt <= 0;
-        dac3_cnt <= 0;
-    end
-    else begin
-        vol_cnt <= vol_cnt + 6'd31;
-        vol0_en <= vol_cnt < gs_vol0;
-        vol1_en <= vol_cnt < gs_vol1;
-        vol2_en <= vol_cnt < gs_vol2;
-        vol3_en <= vol_cnt < gs_vol3;
-        if (vol0_en) dac0_cnt <= dac0_cnt[7:0] + gs_dac0; else dac0_cnt[8] <= 0;
-        if (vol1_en) dac1_cnt <= dac1_cnt[7:0] + gs_dac1; else dac1_cnt[8] <= 0;
-        if (vol2_en) dac2_cnt <= dac2_cnt[7:0] + gs_dac2; else dac2_cnt[8] <= 0;
-        if (vol3_en) dac3_cnt <= dac3_cnt[7:0] + gs_dac3; else dac3_cnt[8] <= 0;
-    end
+        gs_flag_cmd <= vol3[5];
 end
 
 /* GS BUS CONTROLLER */
-assign n_grom = (~n_gmreq && ((ga[15:14] == 2'b00) || (ga[15] == 1'b1 && gs_page == 0)))? 1'b0 : 1'b1;
-assign n_gram = (~n_gmreq && n_grom)? 1'b0 : 1'b1;
-assign gma = (ga[15] == 1'b0)? 4'b0001 : gs_page;
+assign n_grom = (~n_gmreq && ((ga[15:14] == 2'b00) || (ga[15] && gs_page == 0)))? 1'b0 : 1'b1;
+assign n_gram1 = (~n_gmreq && n_grom && (~gs_page[4] || ~ga[15]))? 1'b0 : 1'b1;
+assign n_gram2 = (~n_gmreq && n_grom &&   gs_page[4] &&  ga[15] )? 1'b0 : 1'b1;
+assign gma = (ga[15] == 1'b0)? 4'b0001 : gs_page[3:0];
 assign gd =
     (~n_giorq && ~n_grd && ga[3:0] == 4'h4)? gs_status :
     (~n_giorq && ~n_grd && ga[3:0] == 4'h2)? gs_regb3 :
@@ -262,25 +251,93 @@ assign gd =
                                              {8{1'bz}} ;
 
 
+/* SOUNDRIVE */
+wire port_xf = sd_ena && a[7] == 1'b0 && a[5] == 1'b0 && a[3:0] == 4'hF && !rom_m1_access;
+wire [1:0] port_xf_chn = {a[6],a[4]};
+reg sd_dac0_cs; always @(posedge clk32) sd_dac0_cs = ioreq && port_xf && port_xf_chn == 2'd0;
+reg sd_dac1_cs; always @(posedge clk32) sd_dac1_cs = ioreq && port_xf && port_xf_chn == 2'd1;
+reg sd_dac2_cs; always @(posedge clk32) sd_dac2_cs = ioreq && port_xf && port_xf_chn == 2'd2;
+reg sd_dac3_cs; always @(posedge clk32) sd_dac3_cs = ioreq && port_xf && port_xf_chn == 2'd3;
+wire sd_dac0_wr = sd_dac0_cs && ~n_wr;
+wire sd_dac1_wr = sd_dac1_cs && ~n_wr;
+wire sd_dac2_wr = sd_dac2_cs && ~n_wr;
+wire sd_dac3_wr = sd_dac3_cs && ~n_wr;
+
+
+/* DAC */
+reg [5:0] vol0, vol1, vol2, vol3;
+always @(posedge clk32 or negedge rst_n) begin
+    if (!rst_n) begin
+        vol0 <= 0;
+        vol1 <= 0;
+        vol2 <= 0;
+        vol3 <= 0;
+    end
+    else begin
+        if      (sd_dac0_wr) vol0 <= 6'b111111;
+        else if (gs_vol0_wr) vol0 <= gd[5:0];
+        if      (sd_dac1_wr) vol1 <= 6'b111111;
+        else if (gs_vol1_wr) vol1 <= gd[5:0];
+        if      (sd_dac2_wr) vol2 <= 6'b111111;
+        else if (gs_vol2_wr) vol2 <= gd[5:0];
+        if      (sd_dac3_wr) vol3 <= 6'b111111;
+        else if (gs_vol3_wr) vol3 <= gd[5:0];
+    end
+end
+
+reg [7:0] dac0, dac1, dac2, dac3;
+always @(posedge clk32 or negedge rst_n) begin
+    if (!rst_n) begin
+        dac0 <= 0;
+        dac1 <= 0;
+        dac2 <= 0;
+        dac3 <= 0;
+    end
+    else begin
+        // quartus bug(?): without second condition inside "IF" expression incorrect design may be generated
+        if (sd_dac0_wr && !gs_dac0_wr) dac0 <= ( d[7]?  d : { d[7], ~d[6:0]});
+        else if           (gs_dac0_wr) dac0 <= (gd[7]? gd : {gd[7],~gd[6:0]});
+        if (sd_dac1_wr && !gs_dac1_wr) dac1 <= ( d[7]?  d : { d[7], ~d[6:0]});
+        else if           (gs_dac1_wr) dac1 <= (gd[7]? gd : {gd[7],~gd[6:0]});
+        if (sd_dac2_wr && !gs_dac2_wr) dac2 <= ( d[7]?  d : { d[7], ~d[6:0]});
+        else if           (gs_dac2_wr) dac2 <= (gd[7]? gd : {gd[7],~gd[6:0]});
+        if (sd_dac3_wr && !gs_dac3_wr) dac3 <= ( d[7]?  d : { d[7], ~d[6:0]});
+        else if           (gs_dac3_wr) dac3 <= (gd[7]? gd : {gd[7],~gd[6:0]});
+    end
+end
+
+reg vol0_en, vol1_en, vol2_en, vol3_en;
+reg [5:0] vol_cnt;
+reg [7:0] dac0_cnt, dac1_cnt, dac2_cnt, dac3_cnt;
+assign dac0_out = dac0_cnt[7]? dac0[7] : clk32;
+assign dac1_out = dac1_cnt[7]? dac1[7] : clk32;
+assign dac2_out = dac2_cnt[7]? dac2[7] : clk32;
+assign dac3_out = dac3_cnt[7]? dac3[7] : clk32;
+always @(posedge clk32) begin
+    vol_cnt <= vol_cnt + 6'd31;
+    vol0_en <= (vol_cnt < vol0) || (&vol0);
+    vol1_en <= (vol_cnt < vol1) || (&vol1);
+    vol2_en <= (vol_cnt < vol2) || (&vol2);
+    vol3_en <= (vol_cnt < vol3) || (&vol3);
+    if (vol0_en) dac0_cnt <= dac0_cnt[6:0] + dac0[6:0]; else dac0_cnt[7] <= 0;
+    if (vol1_en) dac1_cnt <= dac1_cnt[6:0] + dac1[6:0]; else dac1_cnt[7] <= 0;
+    if (vol2_en) dac2_cnt <= dac2_cnt[6:0] + dac2[6:0]; else dac2_cnt[7] <= 0;
+    if (vol3_en) dac3_cnt <= dac3_cnt[6:0] + dac3[6:0]; else dac3_cnt[7] <= 0;
+end
+
+
 /* BUS CONTROLLER */
-assign n_ard = n_rd | n_iorq0;
-assign n_awr = n_wr | n_iorq0;
-
-assign aa0 = n_iorq? aa0 : a[1]? saa_a0 : ym_a0 ;
-
-assign ad =
-    ~n_awr && ~n_iorq0 && (port_fffd || port_bffd || port_ff)? d :
-    8'bzzzzzzzz;
-
+assign n_ard = ~ioreq_rd;
+assign n_awr = ~ioreq_wr;
+assign aa0 = a[1]? saa_a0 : ym_a0 ;
+assign ad = ioreq_wr && (port_fffd || port_bffd || port_ff)? d : 8'bzzzzzzzz;
 
 assign n_wait = 1'bz;
-
-assign n_iorqge = (n_m1 && (port_fffd_full || port_bffd || port_b3 || port_bb))? 1'b1 : 1'bz;
-
+assign n_iorqge = (n_m1 && (port_fffd_full || port_bffd || port_b3 || port_bb || port_ff || port_xf))? 1'b0 : 1'b1;
 assign d =
-    ~n_rd && ~n_iorq0 && port_fffd_full? ad :
-    ~n_rd && ~n_iorq0 && port_b3? gs_reg03 :
-    ~n_rd && ~n_iorq0 && port_bb? gs_status :
+    ioreq_rd && port_fffd? ad :
+    ioreq_rd && port_b3? gs_reg03 :
+    ioreq_rd && port_bb? gs_status :
     8'bzzzzzzzz;
 
 
--- cpld/syn/clocks.sdc
+++ cpld/syn/clocks.sdc
@@ -1,3 +1,5 @@
 create_clock -period 32.1MHz -name {clk_32mhz} [get_ports {clk32}]
 
 create_generated_clock -name {gclk} -divide_by 2 -source [get_ports {clk32}] [get_registers {midi_clk_cnt[2]}]
+
+set_false_path -from [get_ports {cfg[*]}]
